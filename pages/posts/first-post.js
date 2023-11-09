05678d248ce012d1936579dd44006df936bc7767
Nathan Soussana <nathan.soussana@gmail.com>
2023-11-09 11:49:49 +0100

    Learned how to add links and images

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,13 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
 export default function FirstPost() {
-  return <h1>First Post</h1>
+  return (
+  <>
+    <h1>First Post</h1>
+    <h2>
+      <Link href="/">Back to home</Link>
+    </h2>
+  </>
+  )
 }
